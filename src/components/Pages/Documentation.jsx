79cd4c8446b304c26758b7ca96a4605966a1cf15
Unmesh Ghosh <151871475+Unmesh100@users.noreply.github.com>
2024-11-30 23:40:53 +0530

    Update Documentation.jsx

--- src/components/Pages/Documentation.jsx
+++ src/components/Pages/Documentation.jsx
@@ -4,17 +4,11 @@ export default function Documentation() {
       <section className="my-4">
         <h1 className="fw-bold mb-3">Documentation 📝</h1>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus
-          neque doloremque quasi, dolores magnam tempora maiores ipsum maxime
-          odit vero, nihil, minima vel suscipit. Ad tempora veniam ipsum iste
-          iusto nesciunt, nihil enim deserunt ducimus facere minima maiores
-          fugit earum excepturi. Illum alias ullam incidunt, explicabo sapiente
-          quo ipsum nulla error tenetur iure repellat expedita magni nam eius
-          vitae illo dolores accusantium autem rerum. Necessitatibus impedit
-          sunt temporibus facilis porro nam nobis ab aperiam ipsum. Voluptate
-          blanditiis voluptatum pariatur, dignissimos, quae quis sequi veritatis
-          accusamus optio, sed officia! Ducimus repellendus ab incidunt nesciunt
-          sunt maxime, voluptatem quibusdam qui fugit quidem?
+          The customer himself, the customer will be able to pursue the adipiscing of the company. We can neither suffer nor suffer as it were, for the great pains of the great times he hates the most; 
+          At times they do not know that this will come to them just, for they do not leave anything, we lead them to make the smallest escapes from them. 
+          If they fall into any other case, I will explain to the wise man that no error is bound by the right to repel the great expedients of his life, but the pains of those who accuse him.
+          Necessities are hindered by easy times for us to open it up. Let the pleasures of flattery be borne by the pleasures, worthy ones, which we accuse one of following the truth as an option, but duties! We lead to be repulsed by the incident they do not know are the most, the pleasure of some who actually runs away?
+
         </p>
       </section>
     </>
